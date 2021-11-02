65c4b7152eb53420953b345d47401f79c91fca9b
aeonux <aeonux@gmail.com>
2021-11-02 19:29:32 -0400

    Monthly report added.

--- src/Controller/TimeLogController.php
+++ src/Controller/TimeLogController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use DoctrineExtensions\Query\Mysql\Month;
+use DoctrineExtensions\Query\Mysql\Year;
 
 /**
  * @Route("/time/log")
@@ -22,9 +24,17 @@ class TimeLogController extends AbstractController
     {
 
         $latest_time_log = $timeLogRepository->findBy(array(),array('id'=>'DESC'),1,0);
+        $monthly_report = $timeLogRepository
+        ->createQueryBuilder('tl')
+        ->select(' SUM(tl.duration) as duration, MONTH(tl.work_day) as month, YEAR(tl.work_day) as year')
+        ->groupBy('month')
+        ->addGroupBy('year')
+        ->getQuery()
+        ->getResult();
         return $this->render('time_log/index.html.twig', [
             'time_logs' => $timeLogRepository->findAll(),
             'latest' => reset($latest_time_log),
+            'monthly_report' => $monthly_report,
         ]);
     }
 
@@ -61,6 +71,9 @@ class TimeLogController extends AbstractController
     {
         $latest_time_log = $timeLogRepository->findBy(array(),array('id'=>'DESC'),1,0);
         $latest_time_log = reset($latest_time_log);
+        if(!$latest_time_log||!is_null($latest_time_log->getEndTime())){
+            throw new \Exception("You must start a new work first.");
+        }
         if(!$latest_time_log||!is_null($latest_time_log->getEndTime())){
             throw new \Exception("You must start a new work first.");
         }
