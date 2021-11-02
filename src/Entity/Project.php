d9f31fdd94acad13810981b5bc16a56bdd01c749
Emir Onuk <e.onuk@outlook.com>
2021-11-02 17:24:43 +0100

    Entities created

---
+++ src/Entity/Project.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjectRepository::class)
+ */
+class Project
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $project_name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $project_description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProjectName(): ?string
+    {
+        return $this->project_name;
+    }
+
+    public function setProjectName(string $project_name): self
+    {
+        $this->project_name = $project_name;
+
+        return $this;
+    }
+
+    public function getProjectDescription(): ?string
+    {
+        return $this->project_description;
+    }
+
+    public function setProjectDescription(?string $project_description): self
+    {
+        $this->project_description = $project_description;
+
+        return $this;
+    }
+}
---
+++ src/Entity/TimeLog.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TimeLogRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TimeLogRepository::class)
+ */
+class TimeLog
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $work_day;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $start_time;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $end_time;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWorkDay(): ?\DateTimeInterface
+    {
+        return $this->work_day;
+    }
+
+    public function setWorkDay(\DateTimeInterface $work_day): self
+    {
+        $this->work_day = $work_day;
+
+        return $this;
+    }
+
+    public function getStartTime(): ?\DateTimeInterface
+    {
+        return $this->start_time;
+    }
+
+    public function setStartTime(?\DateTimeInterface $start_time): self
+    {
+        $this->start_time = $start_time;
+
+        return $this;
+    }
+
+    public function getEndTime(): ?\DateTimeInterface
+    {
+        return $this->end_time;
+    }
+
+    public function setEndTime(?\DateTimeInterface $end_time): self
+    {
+        $this->end_time = $end_time;
+
+        return $this;
+    }
+}
